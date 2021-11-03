de123dcbef77bef7271d8926920346c6a38531ac
TejasASDF <tejasnambiar32@gmail.com>
2021-11-03 14:04:59 +0530

    Security_Tasks

---
+++ Java/SpringSecurity/Assignment4/src/main/java/com/Security/Assignment4/Assignment41Application.java
@@ -0,0 +1,13 @@
+package com.Security.Assignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment41Application.class, args);
+	}
+
+}
